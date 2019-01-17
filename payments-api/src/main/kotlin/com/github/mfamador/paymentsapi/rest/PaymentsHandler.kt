7b2e3a5ca66f276a8312df8522dc1a3615899d19
Marco Amador <marco.amador@meltwater.com>
2019-01-17 21:47:04 +0100

    Add functional endpoints

---
+++ payments-api/src/main/kotlin/com/github/mfamador/paymentsapi/rest/PaymentsHandler.kt
@@ -0,0 +1,41 @@
+package com.github.mfamador.paymentsapi.rest
+
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+
+@Component
+class PaymentsHandler {
+
+    fun getPayments(request: ServerRequest): Mono<ServerResponse> {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromObject("get all!"))
+    }
+
+    fun getPayment(request: ServerRequest): Mono<ServerResponse> {
+        val id = request.pathVariable("id")
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromObject("get $id"))
+    }
+
+    fun addPayment(request: ServerRequest): Mono<ServerResponse> {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromObject("add !"))
+    }
+
+    fun updatePayment(request: ServerRequest): Mono<ServerResponse> {
+        val id = request.pathVariable("id")
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromObject("update $id"))
+    }
+
+    fun deletePayment(request: ServerRequest): Mono<ServerResponse> {
+        val id = request.pathVariable("id")
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+            .body(BodyInserters.fromObject("delete $id"))
+    }
+}
