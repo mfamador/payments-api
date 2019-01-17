7c2363b01e2543152e7fa8f69c8cd9eb66209d89
Marco Amador <marco.amador@meltwater.com>
2019-01-17 21:47:37 +0100

    Add functional endpoints (#1)

---
+++ payments-api/src/main/kotlin/com/github/mfamador/paymentsapi/rest/PaymentsRouter.kt
@@ -0,0 +1,23 @@
+package com.github.mfamador.paymentsapi.rest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class PaymentsRouter(private val handler: PaymentsHandler) {
+
+    @Bean
+    fun route() = router {
+        accept(APPLICATION_JSON).nest {
+            "/payments".nest {
+                GET("/", handler::getPayments)
+                POST("/", handler::addPayment)
+                PUT("/{id}", handler::updatePayment)
+                GET("/{id}", handler::getPayment)
+                DELETE("/{id}", handler::deletePayment)
+            }
+        }
+    }
+}
