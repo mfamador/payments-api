92deb08b4186eac718bcda8d5286bed2ea28d0c4
Marco Amador <marco.amador@meltwater.com>
2019-01-18 00:09:53 +0100

    Fix junit5 dependency

--- payments-api/src/test/kotlin/com/github/mfamador/paymentsapi/PaymentsTests.kt
+++ payments-api/src/test/kotlin/com/github/mfamador/paymentsapi/PaymentsTests.kt
@@ -1,7 +1,7 @@
 package com.github.mfamador.paymentsapi
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
