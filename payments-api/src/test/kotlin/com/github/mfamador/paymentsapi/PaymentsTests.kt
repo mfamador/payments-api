59f5e6fa7b1af8162d8aee122c332389f7630569
Marco Amador <marco.amador@meltwater.com>
2019-01-18 19:56:44 +0100

    Begin integration tests

--- payments-api/src/test/kotlin/com/github/mfamador/paymentsapi/PaymentsTests.kt
+++ payments-api/src/test/kotlin/com/github/mfamador/paymentsapi/PaymentsTests.kt
@@ -1,19 +1,42 @@
 package com.github.mfamador.paymentsapi
 
-import org.assertj.core.api.Assertions.assertThat
+import com.github.mfamador.paymentsapi.model.PaymentOperation
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class PaymentsTests {
+class PaymentsTests(@Value("\${local.server.port}") private val port: Int) {
+
+    val testClient = WebTestClient
+        .bindToServer()
+        .baseUrl("http://localhost:$port")
+        .build()
 
     @Test
-    fun `Assert something`() {
+    fun testGetOperations() {
 
-        println(">> First test")
-        assertThat(1).isEqualTo(1)
+        testClient.get().uri("/payments")
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .exchange()
+            .expectStatus().isOk
+    }
+
+    @Test
+    fun testPostOperation() {
+        val request = PaymentOperation(1, "operation #1")
+
+        testClient.post().uri("/payments")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .body(Mono.just(request), PaymentOperation::class.java)
+            .exchange()
+            .expectStatus().isOk
     }
 }
