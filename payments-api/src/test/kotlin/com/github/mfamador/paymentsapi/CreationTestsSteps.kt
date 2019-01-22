62ea99d1a92b1aeaae7f294f67e6da16c3d3f4e2
Marco Amador <marco.amador@meltwater.com>
2019-01-22 23:06:37 +0100

    Another scenario

--- payments-api/src/test/kotlin/com/github/mfamador/paymentsapi/CreationTestsSteps.kt
+++ payments-api/src/test/kotlin/com/github/mfamador/paymentsapi/CreationTestsSteps.kt
@@ -1,9 +1,6 @@
 package com.github.mfamador.paymentsapi
 
 import com.github.mfamador.paymentsapi.model.Payment
-import cucumber.api.java.en.Given
-import cucumber.api.java.en.Then
-import cucumber.api.java.en.When
 import cucumber.api.java8.En
 import java.util.UUID
 import kotlin.test.assertEquals
@@ -12,20 +9,36 @@ class CreationTestsSteps : En, BaseTest() {
 
     var count = 0L
 
-    @Given("^a file with payment resources in json$")
-    fun readResources() {
-        count = service.count().block()
-    }
-
-    @When("^use them to create payment resources$")
-    fun createResources() {
-        paymentList().data.forEach {
-            service.savePayment(Payment(UUID.randomUUID().toString(), it))!!.block()
+    init {
+        Given("^a file with payment resources in json$") {
+            count = service.count().block()
         }
-    }
+        When("^use them to create payment resources$") {
+            paymentList().data.forEach {
+                service.savePayment(Payment(UUID.randomUUID().toString(), it))!!.block()
+            }
+        }
+        Then("^the resources are created$") {
+            assertEquals(14, service.count().block() - count)
+        }
 
-    @Then("^the resources are created$")
-    fun resourcesAreCreated() {
-        assertEquals(14, service.count().block()-count)
+
+        Given("^a list of resources$") {
+            paymentList().data.forEach {
+                val p = service.savePayment(Payment(UUID.randomUUID().toString(), it))!!.block()
+            }
+        }
+        When("^the version is set to 2$") {
+            val payments = service.getAllPayments().collectList().block()
+            payments.forEach {
+                service.savePayment(Payment(it.id!!, 2, it))!!.block()
+            }
+        }
+        Then("^the resources are are updated with version 2$") {
+            val updatedPayments = service.getAllPayments().collectList().block()
+            updatedPayments.forEach {
+                assertEquals(2, it.version)
+            }
+        }
     }
 }
--- payments-api/src/test/resources/payments.feature
+++ payments-api/src/test/resources/payments.feature
@@ -4,3 +4,10 @@ Feature: Payments
     Given a file with payment resources in json
     When use them to create payment resources
     Then the resources are created
+
+  Scenario: Update payment resources
+    Given a list of resources
+    When the version is set to 2
+    Then the resources are are updated with version 2
+
+
